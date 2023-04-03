036797513650764620350eea7ed8dd7dbbf9e98c
DenisY <kalimm@bk.ru>
2023-04-03 17:48:12 +0300

    Working whith adaptive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-(function () {
-    const header = document.querySelector('.header');
-    window.onscroll = () => {
-        if (window.pageYOffset > 50) {
-            header.classList.add('header_active');
-        } else {
-            header.classList.remove('header_active');
-        }
-    };
-}());
+function readMore() {
+    var dots = document.getElementById("dots");
+    var more = document.getElementById("more");
+    var btn = document.getElementById("btn");
+    if(dots.style.display === "none") {
+        dots.style.display="inline";
+        btn.innerHTML="Показать ещё";
+        more.style.display="none";
+    } else {
+        dots.style.display="none";
+        btn.innerHTML="Скрыть";
+        more.style.display="inline";
+    }
+}
+// function readMore() {
+//     var dots_second = document.getElementById("dots_second");
+//     var more_second = document.getElementById("more_2");
+//     var btn_second = document.getElementById("btn_2");
+//     if(dots_second.style.display === "none") {
+//         dots_second.style.display="inline";
+//         btn_second.innerHTML="Показать ещё";
+//         more_second.style.display="none";
+//     } else {
+//         dots_second.style.display="none";
+//         btn_second.innerHTML="Скрыть";
+//         more_second.style.display="inline";
+//     }
+// }
