2c989e41d80b5fea1a7aedad0f05246184d76b56
DenisY <kalimm@bk.ru>
2023-04-06 17:50:11 +0300

    Seo finish

--- seo/js/main.js
+++ seo/js/main.js
@@ -1,35 +1,3 @@
-//Standart
-function readMoreStandart() {
-    var dots = document.getElementById("dots");
-    var more = document.getElementById("more");
-    var btn = document.getElementById("btn");
-    if(dots.style.display === "none") {
-        dots.style.display="inline";
-        btn.innerHTML="Показать ещё";
-        more.style.display="none";
-    } else {
-        dots.style.display="none";
-        btn.innerHTML="Скрыть";
-        more.style.display="inline";
-    }
-}
-
-//Business
-function readMoreBusiness() {
-    var dots_second = document.getElementById("dots_second");
-    var more_second = document.getElementById("more_second");
-    var btn_second = document.getElementById("btn_second");
-    if(dots_second.style.display === "none") {
-        dots_second.style.display="inline";
-        btn_second.innerHTML="Показать ещё";
-        more_second.style.display="none";
-    } else {
-        dots_second.style.display="none";
-        btn_second.innerHTML="Скрыть";
-        more_second.style.display="inline";
-    }
-}
-
 // Burger handler
 (function () {
     const burgerItem = document.querySelector('.burger');
@@ -50,31 +18,13 @@ function readMoreBusiness() {
         }
     }
 }());
-
-// Popup 
-(function () {
-    const ppBtns = document.querySelectorAll('.popup-act');
-    const ppForm = document.querySelector('.popup_form');
-    const popupClose = document.querySelector('.popup_form-close');
-    ppBtns.forEach((ppBtn) => {
-        ppBtn.addEventListener('click', () => {
-            ppForm.classList.add('popup_form_active');
-        });
-      });
-      popupClose.addEventListener('click', () => {
-        ppForm.classList.remove('popup_form_active');
-    });
-}());
-
-//Swiper
-new Swiper('.swiper', {
-    loop: true,
-    grabCursor: true,
-    slidesPerView: 1,
-    spaceBetween: 12,
-    breakpoints: {
-        640: {
-            slidesPerView: 3,
-        }
-    },
-});
+//Question
+let arrow = document.querySelectorAll('.arrow');
+for (let i = 0; i < arrow.length; i++){
+    let submenu = arrow[i].nextElementSibling;
+   let thisArrow = arrow[i];
+   arrow[i].addEventListener('click',function() {
+       thisArrow.classList.toggle('active');
+       submenu.classList.toggle('open');
+   });
+}
