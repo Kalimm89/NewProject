d0391d91f246848f1f0edad9665b562878412894
DenisY <kalimm@bk.ru>
2023-08-01 16:55:33 +0300

    IIko

---
+++ js/iiko.js
@@ -0,0 +1,24 @@
+//Question
+let arrow = document.querySelectorAll('.arrow');
+for (let i = 0; i < arrow.length; i++){
+    let submenu = arrow[i].nextElementSibling;
+   let thisArrow = arrow[i];
+   arrow[i].addEventListener('click',function() {
+       thisArrow.classList.toggle('active');
+       submenu.classList.toggle('open');
+   });
+};
+// Popup 
+(function () {
+    const ppBtns = document.querySelectorAll('.popup-act');
+    const ppForm = document.querySelector('.popup_form');
+    const popupClose = document.querySelector('.popup_form-close');
+    ppBtns.forEach((ppBtn) => {
+        ppBtn.addEventListener('click', () => {
+            ppForm.classList.add('popup_form_active');
+        });
+      });
+      popupClose.addEventListener('click', () => {
+        ppForm.classList.remove('popup_form_active');
+    });
+}());
